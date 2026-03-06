fd8b217955a3302afb665fd5d24e88bf3112791f
Rasmus Kuschel <rasmushofmann@gmail.com>
2026-03-06 09:05:10 +0100

    Removed mocked endpoints

--- src/main/kotlin/fritzctl/Main.kt
+++ src/main/kotlin/fritzctl/Main.kt
@@ -2,11 +2,9 @@ package fritzctl
 
 import com.github.ajalt.clikt.core.subcommands
 import fritzctl.commands.auth.AuthCommand
-import fritzctl.commands.device.DeviceCommand
-import fritzctl.commands.network.NetworkCommand
 
 fun main(args: Array<String>) {
     FritzCtl()
-        .subcommands(AuthCommand(), DeviceCommand(), NetworkCommand())
+        .subcommands(AuthCommand())
         .main(args)
 }
